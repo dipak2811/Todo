5d0c214be66ac0affdec27ad558e9dbf5ce8d0b8
dipak2811 <gorfadd123@gmail.com>
2023-03-30 17:43:22 +0530

    Deplyment done on gh-pages using CLI

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Input  from "./Component/Input";
+
+function App() {
+  return (
+    <Input />
+    
+  );
+}
+
+export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,45 @@
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+function getDayName(date = new Date(), locale = "en-US") {
+  return date.toLocaleDateString(locale, { weekday: "long" });
+}
+
+function Header() {
+  const date = new Date();
+  const day = getDayName();
+  const tdate = date.getDate();
+  const cmonth = month[date.getMonth()];
+  const year = date.getFullYear();
+  return (
+    <div className="row justify-content-center">
+      <div className="col-12 col-sm-6 col-md-4 Main1">
+        <span className="d-flex justify-content-between px-5">
+          <span className="d-flex">
+            <div className="date">{tdate}</div>
+            <div className="mt-2">
+              <div className="">
+                <strong>{cmonth.substring(0, 3)}</strong>
+              </div>
+              <div className="">{year}</div>
+            </div>
+          </span>
+          <div className="mt-3">{day}</div>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
